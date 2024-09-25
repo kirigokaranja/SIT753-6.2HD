9fcd72505a7bfe46525838278d86654cb1170f90
kirigo <sharonkirigo@gmail.com>
2024-09-25 16:56:28 +1000

    file size

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
           class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
         >
           <div class="mx-auto max-w-xl text-center">
-            <h1 class="text-3xl font-extrabold sm:text-5xl">
+            <h1 class="text-3xl font-extrabold sm:text-4xl">
               Understand User Flow.
               <strong class="font-extrabold text-green-700 sm:block">
                 Increase Conversion.
