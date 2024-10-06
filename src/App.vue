fcce5a2201e223fb9deb25e911845c13bd99c64f
kirigo <sharonkirigo@gmail.com>
2024-10-06 20:06:10 +1100

    hero section

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,60 @@
 <template>
   <div>
-    <div>
-      <section class="bg-gray-50">
+    <!-- component -->
+    <section>
+      <div class="bg-black text-white py-20">
         <div
-          class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+          class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24"
         >
-          <div class="mx-auto max-w-xl text-center">
-            <h1 class="text-3xl font-extrabold sm:text-4xl">
-              Understand User Flow.
-              <strong class="font-extrabold text-green-700 sm:block">
-                Increase Conversion.
-              </strong>
+          <div
+            class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8"
+          >
+            <h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
+              TechFest
             </h1>
-
-            <p class="mt-4 sm:text-xl/relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
+            <h2
+              class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2"
+            >
+              Space : The Timeless Infinity
+            </h2>
+            <p class="text-sm md:text-base text-gray-50 mb-4">
+              Explore your favourite events and register now to showcase your
+              talent and win exciting prizes.
             </p>
-
-            <div class="mt-8 flex flex-wrap justify-center gap-4">
-              <a
-                class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                href="#"
-              >
-                Get Started
-              </a>
-
-              <a
-                class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-                href="#"
-              >
-                Learn More
-              </a>
+            <a
+              href="#"
+              class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
+            >
+              Explore Now</a
+            >
+          </div>
+          <div
+            class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center"
+          >
+            <div class="h-48 flex flex-wrap content-center">
+              <div>
+                <img
+                  class="inline-block mt-28 hidden xl:block"
+                  src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
+                />
+              </div>
+              <div>
+                <img
+                  class="inline-block mt-24 md:mt-0 p-8 md:p-0"
+                  src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
+                />
+              </div>
+              <div>
+                <img
+                  class="inline-block mt-28 hidden lg:block"
+                  src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   </div>
 </template>
 
